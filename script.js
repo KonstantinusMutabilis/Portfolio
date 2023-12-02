e77e3511de42623dbd396f6116cc1d43f708b55b
KonstantinusMutabilis <b.d.acc.38@gmail.com>
2023-12-02 19:58:55 +0200

    new file:   Credit Card/creditcard.html 	new file:   Memory Game/memorygame.html 	new file:   Page Builder/pagebuilder.html 	new file:   Users Manager/usersmanager.html 	new file:   img/JSProjects/CreditCard.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const jsworks = document.querySelector(".jsworks");
 
 
 
-function Project(name, description, image, page, code) {
-    this.name = name,
+function Project(id, name, description, image, page, code, way) {
+    this.id = id,
+        this.name = name,
         this.description = description,
         this.image = image,
         this.page = page,
-        this.code = code
+        this.code = code,
+        this.way = way
 }
 
 const usersManager = new Project(
+    0,
     "Users Manager",
     "This is a project that Manage Users",
     {
@@ -19,10 +22,13 @@ const usersManager = new Project(
         alt: "Users Manager"
     },
     "https://konstantinusmutabilis.github.io/Users-Manager/",
-    "https://github.com/KonstantinusMutabilis/Users-Manager/"
+    "https://github.com/KonstantinusMutabilis/Users-Manager/",
+    "./Users Manager/usersmanager.html"
+
 );
 
 const pageBuilder = new Project(
+    1,
     "Page Builder",
     "This Project is tool pallete for creating new html elements",
     {
@@ -30,10 +36,12 @@ const pageBuilder = new Project(
         alt: "Page Builder"
     },
     "https://konstantinusmutabilis.github.io/Page-Builder/",
-    "https://github.com/KonstantinusMutabilis/Page-Builder/"
+    "https://github.com/KonstantinusMutabilis/Page-Builder/",
+    "./Page Builder/pagebuilder.html"
 );
 
 const memoryGame = new Project(
+    2,
     "Memory Game",
     "This is memory game where you should chuse 2 cards, and they must be the same color",
     {
@@ -41,19 +49,35 @@ const memoryGame = new Project(
         alt: "Memory Game"
     },
     "https://konstantinusmutabilis.github.io/Memory-Game/",
-    "https://github.com/KonstantinusMutabilis/Memory-Game/"
+    "https://github.com/KonstantinusMutabilis/Memory-Game/",
+    "./Memory Game/memorygame.html"
+
+)
+
+const creditCard = new Project(
+    3,
+    "Credit Card",
+    "This is credit card form that sends data to email after filling all fields right way",
+    {
+        url: "./img/JSProjects/CreditCard.png",
+        alt: "Credit Card"
+    },
+    "",
+    "",
+    "./Credit Card/creditcard.html"
 
 )
 
 const projectsArray = new Array();
 
-projectsArray.push(usersManager, pageBuilder, memoryGame);
+projectsArray.push(usersManager, pageBuilder, memoryGame, creditCard);
 
 function cardCreation() {
 
+
     projectsArray.forEach((element) => {
         const card = document.createElement('div');
-        card.classList.add('card');
+        card.classList.add('card', 'js');
         let img = document.createElement("img");
         img.setAttribute('src', element.image.url);
         img.setAttribute('alt', element.image.alt);
@@ -61,21 +85,105 @@ function cardCreation() {
         card.appendChild(img);
         let desc = document.createElement('div');
         desc.classList.add('card-description');
-        let h = createElement('h3');
+        let h = document.createElement('h3');
         h.textContent = element.name;
         desc.appendChild(h);
-        let p = createElement('p');
+        let p = document.createElement('p');
         p.textContent = element.description;
         desc.appendChild(p);
         card.appendChild(desc);
         let buttonDiv = document.createElement('div');
         buttonDiv.classList.add('card-button');
+        let a = document.createElement('a');
+        a.setAttribute('href', element.way);
+        a.setAttribute('target', "_blank");
+        buttonDiv.appendChild(a);
         let button = document.createElement('input');
+        button.setAttribute('type', "button");
         button.setAttribute('value', 'לפרויקט');
-        buttonDiv.appendChild(button);
+        button.classList.add('jsbutton');
+        a.appendChild(button);
         card.appendChild(buttonDiv);
         jsworks.appendChild(card);
     });
 
 }
+
+
+function popUp() {
+    let cardslist = document.querySelectorAll('.js');
+    let cards = [...cardslist];
+
+    let Project = document.querySelector(".Project");
+    let Display = document.getElementById("modal");
+
+    cards.forEach(card => {
+
+
+
+        document.querySelector('.jsbutton').addEventListener("keyup", function add(event) {
+            event.preventDefault();
+
+
+
+            console.log("works");
+            Display.style.display = "flex";
+
+            let img = document.createElement("img");
+            img.setAttribute('src', card.img.url);
+            img.setAttribute('alt', card.img.alt);
+
+            Project.appendChild(img);
+            let desc = document.createElement('div');
+            desc.classList.add('card-description');
+            let h = document.createElement('h3');
+            h.textContent = card.name;
+            desc.appendChild(h);
+            let p = document.createElement('p');
+            p.textContent = element.description;
+            desc.appendChild(p);
+            Project.appendChild(desc);
+            let buttonDiv = document.createElement('div');
+            buttonDiv.classList.add('card-button');
+
+            let a1 = document.createElement('a');
+            a1.setAttribute('href', projectsArray[i].code);
+            a1.setAttribute('target', "_blank");
+            buttonDiv.appendChild(a1);
+            let button1 = document.createElement('input');
+            button1.setAttribute('type', "button");
+            button1.setAttribute('value', "להורדת פרויקט");
+            a1.appendChild(button1);
+            let a2 = document.createElement('a');
+            a2.setAttribute('href', projectsArray[i].page);
+            a2.setAttribute('target', "_blank");
+            buttonDiv.appendChild(a2);
+            let button2 = document.createElement('input');
+            button2.setAttribute('type', "button");
+            button2.setAttribute('value', "לאתר");
+            a2.appendChild(button2);
+            Project.appendChild(buttonDiv);
+
+
+        });
+
+
+
+    })
+
+
+    window.onclick = function (e) {
+        if (e.target === Display) {
+            Display.style.display = "none";
+            Project = "";
+
+        }
+    };
+
+
+}
+
+
 cardCreation();
+
+popUp();
